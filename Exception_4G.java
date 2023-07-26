192b8d6aecd55cab7d41e2c1bfbfb8cb458a6046
Thrisha999 <101519219+Thrisha999@users.noreply.github.com>
2023-07-26 12:53:31 +0530

    Add files via upload

---
+++ Exception_4G.java
@@ -0,0 +1,15 @@
+class Ambika extends Exception
+{
+    Ambika(String s)
+    {
+        super(s);
+    }
+}
+
+public class Exception_4G 
+{
+    public static void main(String args[])
+    {
+
+    } 
+}
